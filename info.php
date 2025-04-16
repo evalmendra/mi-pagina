8d1946c30aaa81b273b1964b5a52029a4fc3f681
Eva Bernat <evalmendra@gmail.com>
2025-04-16 20:43:43 +0200

    Actualización del contenido de la web

--- info.php
+++ info.php
@@ -1,3 +1,12 @@
 <?php
-phpinfo();
+$to = "evalmendra@gmail.com"; // Cámbialo por uno tuyo real
+$subject = "Test de correo desde PHP";
+$message = "Este es un mensaje de prueba desde el servidor.";
+$headers = "From: noreply@tudominio.com";
+
+if (mail($to, $subject, $message, $headers)) {
+  echo "Correo enviado correctamente.";
+} else {
+  echo "Error al enviar el correo.";
+}
 ?>
